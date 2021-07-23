de8bfd095e0a2ecd96b1078886ea8a1aea4d2c65
Brad J <j.chaiyaphop@gmail.com>
2021-07-24 01:04:32 +0700

    Adjust the input line

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from render import render_table
 def main():
     books = load_db('./helper_files/book-db.csv')
     repository = BookRepository(books)
-    inputs = input("Enter Title -> ")
+    inputs = input("Enter the title -> ")
     result: list[Book] = repository.find_length(inputs)
     render_table(result)
 
